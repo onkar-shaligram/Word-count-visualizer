318b0fb5d004a003dd9edff6477d620430107919
onkar-shaligram <onkarshaligram01@gmail.com>
2020-08-30 00:52:33 +0530

    added differnt cases for text filtering on basis of punctuations

--- script.js
+++ script.js
@@ -5,7 +5,23 @@ $('#btnCalc').click(()=> {
     let chars = text.split('');
     let newChars = [];
     chars.forEach(c => {
-        newChars.push(c);
+        switch(c){
+            case ",":
+            case ";":
+            case "'":
+            case "`":
+            case ":":
+            case `"`:
+            case "-":
+            case "_":
+            case "“":
+            case "”":
+            case "!":
+            case ".":
+            case "’":
+            case "?": return;
+            default: newChars.push(c.toLowerCase());
+        }
     });
     let newText = newChars.join('');
     console.log(newText);
